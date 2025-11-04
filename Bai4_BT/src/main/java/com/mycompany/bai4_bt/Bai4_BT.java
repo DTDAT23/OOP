8dfc984936ca22de996a4b5eb4dd0d20ece004c3
DTDAT23 <tiendatdmx2006@gmail.com>
2025-11-04 10:40:15 +0700

    new file

---
+++ Bai4_BT/src/main/java/com/mycompany/bai4_bt/Bai4_BT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bai4_bt;
+
+/**
+ *
+ * @author tiend
+ */
+public class Bai4_BT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
